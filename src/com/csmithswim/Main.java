7887e38deda6ec0be33ef2a0386defab3bc61c80
Cullen S <cullensmi@gmail.com>
2020-12-26 21:38:22 -0500

    solved algorithm

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,12 +1,22 @@
+package com.csmithswim;
 import java.io.*;
 import java.util.*;
+
 
 interface AdvancedArithmetic{
     int divisorSum(int n);
 }
 class Calculator implements AdvancedArithmetic {
     public int divisorSum(int n) {
-        return 0;
+        Integer sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n%i == 0){
+                sum += i;
+//                System.out.println(sum);
+            }
+        }
+//        System.out.println(sum);
+        return sum;
     }
 }
 
